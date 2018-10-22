18640729826832d2f0d8736985ff6f075cc0e268
Michael Hibbard <38772539+mjhibbard@users.noreply.github.com>
2018-10-21 19:52:23 -0700

    cleanup and remove all bootstrap classes/structure

--- public/app.js
+++ public/app.js
@@ -2,20 +2,20 @@
 //     alert("connected");
 // });
 
-$(function () {
-    $('[data-toggle="popover"]').popover()
-  })
+// $(function () {
+//     $('[data-toggle="popover"]').popover()
+//   })
 
-$('.popover-dismiss').popover({
-    trigger: 'focus'
-  })
+// $('.popover-dismiss').popover({
+//     trigger: 'focus'
+//   })
 
-  $(document).ready(function(){
-    $('.dropdown-submenu a.test').on("click", function(e){
-      $(this).next('ul').toggle();
-      e.stopPropagation();
-      e.preventDefault();
-    });
-  });
+//   $(document).ready(function(){
+//     $('.dropdown-submenu a.test').on("click", function(e){
+//       $(this).next('ul').toggle();
+//       e.stopPropagation();
+//       e.preventDefault();
+//     });
+//   });
   
   
