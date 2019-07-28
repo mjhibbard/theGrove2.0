68b7cd11500dc9ea6ae1eb4b2386ae73d9f975e2
Michael <veg.it.up@live.com>
2019-07-28 13:52:51 -0700

    resize imgs, repair vuln, style adustments

--- public/app.js
+++ public/app.js
@@ -1,26 +1,7 @@
-// $(document).on('click', function(){
-//     alert("connected");
-// });
-
-// $(document).ready(function(){
-//     $('nav li ul').hide().removeClass('dropDown-content');
-//     $('nav li').hover(
-//         function () {
-//             $('ul', this).stop().slideDown(500);
-//         },
-//         function () {
-//             $('ul', this).stop().slideUp(300);
-//         }
-//     );
-//     $('#header-bars-toggle').click(function() {
-//         $('#jQ-header__nav-menu').toggle('.active');
-//     });
-// });
-
 function initNav() {
-  // makeNavToggler :: HTMLElement -> void -> void
+  // Make the NavToggler
   function makeNavToggler(wrap) {
-    // toggleNav :: HTMLElement -> void
+    //Nav Toggling classes and checking
     function toggleNav(wrap) {
       if (wrap.classList.contains("nav-menu__screen--show")) {
         wrap.classList.remove("nav-menu__screen--show");
